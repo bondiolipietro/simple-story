9245e02c093b3bce09ad54ccb4b0cf41b85b792a
Pietro Bondioli <pietrobondiolideveloper@gmail.com>
2022-01-10 01:01:06 -0300

    feat: create 'story' service with mocked responses

---
+++ src/services/StoryService.ts
@@ -0,0 +1,74 @@
+export class StoryService {
+  public async getAllStories(): Promise<Story[]> {
+    let mock = [
+      {
+        id: '1',
+        title: 'title 1',
+        preview_image_url: 'url 1',
+        description: 'description 1',
+        pages: [
+          {
+            text: 'Texto da pagina 1',
+            image_url: 'url 1'
+          },
+          {
+            text: 'Texto da pagina 2',
+            image_url: 'url 2'
+          }
+        ]
+      },
+      {
+        id: '2',
+        title: 'title 2',
+        preview_image_url: 'url 2',
+        description: 'description 2',
+        pages: [
+          {
+            text: 'Texto da pagina 1',
+            image_url: 'url 1'
+          },
+          {
+            text: 'Texto da pagina 2',
+            image_url: 'url 2'
+          }
+        ]
+      }
+    ];
+
+    return mock;
+  }
+
+  public async getStoryById(id: string): Promise<Story> {
+    let mock = {
+      id: '1',
+      title: 'title 1',
+      preview_image_url: 'url 1',
+      description: 'description 1',
+      pages: [
+        {
+          text: 'Texto da pagina 1',
+          image_url: 'url 1'
+        },
+        {
+          text: 'Texto da pagina 2',
+          image_url: 'url 2'
+        }
+      ]
+    };
+
+    return mock;
+  }
+}
+
+export type Story = {
+  id: string;
+  title: string;
+  preview_image_url: string;
+  description: string;
+  pages: StoryPage[];
+};
+
+type StoryPage = {
+  text: string;
+  image_url: string;
+};
